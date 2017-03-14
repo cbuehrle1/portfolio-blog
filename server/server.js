d47f07e46c6e3ffc783f5cc1b683dd1a0d67d065
Chad Buehrle <chad.buehrle@gmail.com>
2017-03-14 12:37:22 -0500

    heroku opt

--- server/server.js
+++ server/server.js
@@ -231,6 +231,8 @@ app.post("/signup", function(req, res, next) {
 
 app.use(require("./api-routes.js")());
 
-app.listen(5000, function() {
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
   console.log('listening on port 5000.');
 });
